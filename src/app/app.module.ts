7b76ecda307b621ac124cfe7bccf1ce3f28a83ed
Nalt0 <aliaksandr2010@yandex.by>
2019-03-08 19:05:38 +0300

    Renamed about us in about eat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material/material.module";
-import { CarAboutUsComponent } from "./car-about-us/car-about-us.component";
+import { CarAboutEatComponent } from "./components/car-about-eat/car-about-eat.component";
 import { MainPageComponent } from "./components/main-page/main-page.component";
 import { OthersModule } from "./others/others.module";
 import { MenuItemService } from "./services/menu-item.service";
 import { MenuComponent } from "./components/menu/menu.component";
 import { NewsComponent } from "./components/news/news.component";
 import { NewsService } from "./services/news/news.service";
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import { FooterComponent } from "./footer/footer.component";
     MainPageComponent,
     MenuComponent,
     NewsComponent,
-    CarAboutUsComponent
+    CarAboutEatComponent
   ],
   imports: [
     BrowserModule,
