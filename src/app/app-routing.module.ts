96a5a0d3b5b29eeb43d11ae635bcfd05f7aba1dc
Kirill <starkeiman@yandex.ru>
2019-03-08 19:31:29 +0300

    added routing for main-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainPageComponent } from "./components/main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/main", pathMatch: "full" },
+  { path: "main", component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
