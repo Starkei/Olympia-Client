2f563f273e2639e20f16d3edd32013b98d08874d
TimRois <andrei_k99@mail.ru>
2019-03-08 20:58:38 +0300

    Adeed nutritionist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material/material.module";
+import { NutritionistComponent } from './components/pages/nutritionist/nutritionist.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NutritionistComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MatButtonModule } from "@angular/material";
+import { MatButtonModule, MatCardModule } from "@angular/material";
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule]
+  exports: [MatButtonModule, MatCardModule]
 })
 export class MaterialModule {}
