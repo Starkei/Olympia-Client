8be345e97c75fb1c968b11a44bfac657ea80973d
TimRois <andrei_k99@mail.ru>
2019-03-08 18:40:34 +0300

    fixed problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material/material.module";
-import { NutritionistComponent } from './pages/nutritionist/nutritionist.component';
-import{FlexLayoutModule} from "@angular/flex-layout";
-import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
-  declarations: [AppComponent, NutritionistComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
