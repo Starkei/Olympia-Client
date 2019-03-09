8e5128ef802587a847020cadcfdb23b36f682248
Kirill <starkeiman@yandex.ru>
2019-03-09 13:39:27 +0300

    congifured routing for component crowdfunding.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainPageComponent } from "./components/main-page/main-page.component";
 import { ShopComponent } from "./components/shop/shop.component";
+import { CrowdfundingComponent } from "./components/crowdfunding/crowdfunding.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/main", pathMatch: "full" },
   { path: "main", component: MainPageComponent },
-  { path: "shop", component: ShopComponent }
+  { path: "shop", component: ShopComponent },
+  { path: "crowdfunding", component: CrowdfundingComponent }
 ];
 
 @NgModule({
