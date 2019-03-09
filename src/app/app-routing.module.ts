141497fcf1cb650a365a3b8472c7a2f64867ca16
Nalt0 <aliaksandr2010@yandex.by>
2019-03-09 19:00:28 +0300

    Update card about eat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainPageComponent } from "./components/main-page/main-page.component";
+import { CarAboutEatComponent } from "./components/car-about-eat/car-about-eat.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/main", pathMatch: "full" },
-  { path: "main", component: MainPageComponent }
+  { path: "main", component: MainPageComponent },
+  { path: "cardabouteat", component: CarAboutEatComponent }
 ];
 
 @NgModule({
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,10 +4,17 @@ import { MatButtonModule } from "@angular/material";
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule, MatToolbarModule, MatCardModule, MatDividerModule]
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatTableModule
+  ]
 })
 export class MaterialModule {}
