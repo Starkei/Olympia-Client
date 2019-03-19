83f303a22f455ae49ce6e3e3e478459e36425944
TimRois <andrei_k99@mail.ru>
2019-03-19 15:46:01 +0300

    tiny change

--- src/app/classes/training/training.ts
+++ src/app/classes/training/training.ts
@@ -1,15 +1,22 @@
 import { Output } from "src/app/interfaces/output";
 
 export class Training implements Output {
-  constructor(
-    public title: string,
-    public description: string,
-    public image: string,
-    public moreInfo: string,
-    public price: number,
-    public leader: string,
-    public currency: string
-  ) {}
+  public title: string;
+  public description: string;
+  public image: string;
+  public moreInfo: string;
+  public price: number;
+  public leader: string;
+  public currency: string;
+  constructor(training: Training) {
+    this.title = training.title;
+    this.description = training.description;
+    this.image = training.image;
+    this.moreInfo = training.moreInfo;
+    this.price = training.price;
+    this.leader = training.leader;
+    this.currency = training.currency;
+  }
 
   public getFormattedPrice(): string {
     return `Цена ${this.price}${this.currency}`;
