634927a15828feeded955145c434c8e64dfc8d5d
Kirill <starkeiman@yandex.ru>
2019-04-09 11:27:57 +0300

    added adware component

---
+++ src/app/components/shared/adware/adware.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adware',
+  templateUrl: './adware.component.html',
+  styleUrls: ['./adware.component.scss']
+})
+export class AdwareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
