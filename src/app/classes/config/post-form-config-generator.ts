36641360f7f44479187bdb44134e0243d8c3dff4
Kirill <Starkeiman@yandex.ru>
2019-06-05 21:34:50 +0300

    Created config generator.

--- src/app/classes/config/post-form-config-generator.ts
+++ src/app/classes/config/post-form-config-generator.ts
@@ -1,16 +1,11 @@
 import { PostFormConfig } from "src/app/interfaces/configs/post-form-config";
 import { ProductsConfig } from "./products/products-config";
-import { ProductsTypesConfig } from "./products/products-types-config";
-import { ProductService } from "src/app/services/product/product.service";
 
 export class PostFormConfigGenerator {
-  constructor(private productConfig: ProductsConfig, private productTypesConfig: ProductsTypesConfig) {}
+  constructor(private productConfig: ProductsConfig) { }
 
   getProductsConfig(): PostFormConfig {
     return this.productConfig.getPostFormConfig();
   }
 
-  getProductsTypesConfig(): PostFormConfig {
-    return this.productTypesConfig.getPostFormConfig();
-  }
 }
