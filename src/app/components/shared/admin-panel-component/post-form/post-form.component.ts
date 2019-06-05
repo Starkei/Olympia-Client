63a3a0ebabff862c23beee2a610dbef9ab09a9bc
Kirill <Starkeiman@yandex.ru>
2019-06-05 21:38:38 +0300

    Modified post form and table.

--- src/app/components/shared/admin-panel-component/post-form/post-form.component.ts
+++ src/app/components/shared/admin-panel-component/post-form/post-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { PostFormConfig } from "src/app/interfaces/configs/post-form-config";
 import { Output } from "src/app/interfaces/output";
+import { Field } from 'src/app/engine/interfaces/field';
 
 @Component({
   selector: "app-post-form",
@@ -10,17 +11,67 @@ import { Output } from "src/app/interfaces/output";
 export class PostFormComponent implements OnInit {
   @Input() config: PostFormConfig;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  checkValid(): boolean {
+    for (const group of this.config.groups) {
+      for (const field of group.fields) {
+        if (field.isInvalid)
+          return false;
+      }
+    }
+    return true;
+  }
 
   postData(): void {
     let output: any = {};
+    let checkboxData: any = {};
     for (const group of this.config.groups) {
       for (const field of group.fields) {
-        output[field.dbFieldName] = field.innerText;
+        switch (field.fieldType) {
+          case "input":
+            output = this.assignValueToDBField(field.innerText, field.dbFieldName, output);
+            break;
+          case "checkbox":
+            if (field.checked) {
+              if (field.dbFieldName && field.dbFieldName[0])
+                if (checkboxData[field.dbFieldName[0]])
+                  checkboxData[field.dbFieldName[0]].push(field.title);
+                else {
+                  checkboxData[field.dbFieldName[0]] = [];
+                  checkboxData[field.dbFieldName[0]].push(field.title);
+                }
+            }
+            break;
+          case "select":
+            output = this.assignValueToDBField(field.innerText, field.dbFieldName, output);
+            break;
+          case "textarea":
+            output = this.assignValueToDBField(field.innerText, field.dbFieldName, output);
+            break;
+          case "button":
+            if (field.buttonType === "file")
+              output = this.assignValueToDBField(field.innerText, field.dbFieldName, output);
+            break;
+        }
       }
     }
+    for (const key in checkboxData) {
+      output = this.assignValueToDBField(checkboxData[key], [key], output);
+    }
     this.config.onPost(output);
+  }
+  private assignValueToDBField(value: any, dbFieldName: Array<string>, output: Output): Output {
+    if (!dbFieldName || !value)
+      return output;
+    if (dbFieldName[1]) {
+      output[dbFieldName[0]] = {};
+      output[dbFieldName[0]][dbFieldName[1]] = value;
+    } else
+      if (dbFieldName[0])
+        output[dbFieldName[0]] = value;
+    return output;
   }
 }
--- src/app/components/shared/admin-panel-component/table/table.component.ts
+++ src/app/components/shared/admin-panel-component/table/table.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from "@angular/core";
-import { MatTableDataSource } from "@angular/material";
+import { Component, OnInit, Input, ChangeDetectorRef, ViewChild } from "@angular/core";
+import { MatTableDataSource, MatSort, MatPaginator } from "@angular/material";
 import { Output } from "src/app/interfaces/output";
-import { SelectionModel, DataSource } from "@angular/cdk/collections";
+import { SelectionModel } from "@angular/cdk/collections";
 import { TableConfig } from "src/app/interfaces/configs/table-config";
-import { config } from "rxjs";
-import { TableDataSource } from "src/app/classes/datasource/table-datasource";
 
 @Component({
   selector: "app-table",
@@ -13,39 +11,90 @@ import { TableDataSource } from "src/app/classes/datasource/table-datasource";
 })
 export class TableComponent implements OnInit {
   displayColumns: Array<string>;
-  dataSource: TableDataSource;
+  dataSource: MatTableDataSource<Output>;
   selection: SelectionModel<Output>;
 
   @Input() config: TableConfig;
 
-  constructor(private changeDetector: ChangeDetectorRef) {}
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit() {
-    if (!config) throw new Error("Config does not exists!");
-    this.displayColumns = ["select", ...this.config.displayColumns];
-    this.dataSource = new TableDataSource(this.config);
+    if (!this.config) throw new Error("Config does not exists!");
+    this.displayColumns = ["select", ...this.config.allColumns];
+    this.config.showAll().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    });
     this.selection = new SelectionModel<Output>(true, []);
-    this.dataSource.loadData();
   }
 
   isAllSelected(): boolean {
-    if (!this.dataSource) return false;
-    return false;
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
 
   masterToggle(): void {
-    if (!this.dataSource) return;
-    return;
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
   checkboxLabel(row?: Output): string {
     if (!row) {
-      return `${this.isAllSelected() ? "select" : "deselect"} all`;
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
-    return `${this.selection.isSelected(row) ? "deselect" : "select"}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'}`;
   }
 
-  refresh(): void {
-    this.dataSource.loadData();
+  deleteAll(): void {
+    this.config.onDelete(this.selection.selected);
+    this.selection.clear();
   }
+
+  getDataFrom(element: any, fields: Array<string>): any {
+    let start: any = element;
+    for (const iterator of fields) {
+      if (element[iterator])
+        element = element[iterator];
+    }
+
+    element = this.convertToNormalDate(element);
+    element = this.convertArrayOfTimeStamp(element);
+
+    if (start === element)
+      element = null;
+
+    return element ? element : "Пусто";
+  }
+
+  applyFilter(value) {
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
+
+  private convertArrayOfTimeStamp(elements): any {
+    if (elements && typeof elements === "object" && elements.length)
+      for (let i = 0; i < elements.length; i++) {
+        elements[i] = this.convertToNormalDate(elements[i]);
+      }
+    return elements;
+  }
+
+  private convertToNormalDate(element): any {
+    if (element && typeof element === "object" && element.seconds) {
+      let options = { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "2-digit", minute: "2-digit" };
+      let withoutHours = { year: "numeric", month: "long", day: "2-digit" };
+      element = element.toDate();
+      if (element.getHours() == "00")
+        element = element.toLocaleString("ru-RU", withoutHours);
+      else
+        element = element.toLocaleString("ru-RU", options);
+    }
+    return element;
+  }
+
 }
