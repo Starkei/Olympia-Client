017b2b19f55871b88ff380632c538acf07ffb90f
Kirill <Starkeiman@yandex.ru>
2019-06-07 00:23:20 +0300

    Tried create tests.

--- src/app/components/pages/chat-page/chat.component.spec.ts
+++ src/app/components/pages/chat-page/chat.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatComponent } from './chat.component';
+import { BannerComponent } from '../../shared/banner/banner.component';
+import { MaterialModule } from 'src/app/modules/material/material.module';
+import { ChatService } from 'src/app/services/chat-service/chat.service';
+import { AuthService } from 'src/app/services/auth/Auth.service';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
@@ -8,9 +17,23 @@ describe('ChatComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatComponent ]
+      imports: [
+        MaterialModule,
+        FormsModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        ChatComponent,
+        BannerComponent
+      ],
+      providers: [
+        ChatService,
+        AuthService,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
